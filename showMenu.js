89dc99f2c7e82d512e8dfb67a59c3da974927305
dfomby <dvash.fomby19@stu.bmcc.cuny.edu>
2025-11-20 00:28:06 -0500

    mockups expand and return when clicked

--- showMenu.js
+++ showMenu.js
@@ -6,3 +6,16 @@ function showMenu() {
         menu.style.display = "none";
     }
 }
+
+
+const images = document.querySelectorAll('.expandableImage');
+
+// Loop through the NodeList of images
+images.forEach(image => {
+  // Add a 'click' event listener to each individual image
+  image.addEventListener('click', function() {
+    // Toggle the 'expanded' class on the *specific* image that was clicked
+    this.classList.toggle('expanded');
+    
+  });
+});
