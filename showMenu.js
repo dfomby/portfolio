6a1435f67cee25a457eccf8f285b63f14c27a8ae
dfomby <dvash.fomby19@stu.bmcc.cuny.edu>
2025-12-26 02:30:23 -0500

    poptcha: text edits, code indentation

--- showMenu.js
+++ showMenu.js
@@ -1,10 +1,10 @@
 function showMenu() {
-    var menu = document.getElementById("top-nav");
-    if (menu.style.display === "none" || menu.style.display === '') {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
+  var menu = document.getElementById("top-nav");
+  if (menu.style.display === "none" || menu.style.display === '') {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
 }
 
 
@@ -13,9 +13,9 @@ const images = document.querySelectorAll('.expandableImage');
 // Loop through the NodeList of images
 images.forEach(image => {
   // Add a 'click' event listener to each individual image
-  image.addEventListener('click', function() {
+  image.addEventListener('click', function () {
     // Toggle the 'expanded' class on the *specific* image that was clicked
     this.classList.toggle('expanded');
-    
+
   });
 });
